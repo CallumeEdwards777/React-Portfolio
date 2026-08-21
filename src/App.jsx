6804de0005dd1e7c11f5479302d07225c47b9404
CallumeEdwards777 <Callum2218@gmail.com>
2026-08-21 23:34:48 +0100

    Set up my import names and created a structure before coding in components and pages . Set up main.jsx to render the application

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import ProjectsPage from './pages/ProjectsPage';
+import AboutPage from './pages/AboutPage';
+
+function App() {
+  const [page, setPage] = useState('home');
+
+  return (
+    <Layout selectedPage={page} onSetPage={setPage}>
+      {page === 'home' && <HomePage />}
+      {page === 'projects' && <ProjectsPage />}
+      {page === 'about' && <AboutPage />}
+    </Layout>
+  );
+}
+
+export default App;
