b8f85e96404101972a7d04b0c6c6756e54e4408d
CallumeEdwards777 <Callum2218@gmail.com>
2026-08-22 12:29:02 +0100

    Set up the react home and about pages, the project page will need to have components written to be exported before forming a page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I am a beginner web developer who is learning the core fundamentals on the step8up bootcamp course, so far I have learned HTML, CSS, Bootstrap, Javascript, Python, APIs, Node.js, MySQL and more. My aspiration at first was to simply learn a new skill and get myself engaging in something new but I potentially I could see myself working in the sector as a junior developer if I enhance my skills after course completion! </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Hi, I'm Callum</h1>
+            <p>Welcome to my Portfolio! This is a showcase of everything I have learned so far on the step8up bootcamp course</p>
+            <p>
+                <a href="https://github.com/CallumeEdwards777" target="_blank" rel="noreferrer">GitHub</a>
+                {' . '}
+                <a href="https://www.linkedin.com/in/callum-edwards-1b0a4b1b3/" target="_blank" rel="noreferrer">LinkedIn</a>
+            </p>
+        </div>
+    );
+};
+
+export default HomePage;
