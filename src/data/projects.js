5e8f01326b3d536f7a969568db1b6f5d30c3f66d
CallumeEdwards777 <Callum2218@gmail.com>
2026-08-22 12:12:57 +0100

    Added in projects to a javascript file that will be exported and used in the in the project card react files

---
+++ src/data/projects.js
@@ -0,0 +1,51 @@
+const projects = [
+  {
+    id: 1,
+    title: 'Week 1 - Callums-awesome-portfolio',
+    description: 'This is my first project built with html and css',
+    tech: 'HTML, CSS',
+    link: 'https://github.com/CallumeEdwards777/Callums-awesome-portfolio',
+  },
+  {
+    id: 2,
+    title: 'Week 2 - First attempt at a portfolio',
+    description: 'This is my second project which introduced more pages that link together and a navigation bar that was styled using new techniques learned in week 2',
+    tech: 'HTML, CSS',
+    link: 'https://github.com/CallumeEdwards777/First-attempt-at-a-full-webpage.git',
+  },
+  {
+    id: 3,
+    title: 'Week 3 - bootstrap page',
+    description: 'My third project introduced bootstrap which is a framework that allows you to build websites faster and easier',
+    tech: 'HTML, CSS, Bootstrap',
+    link: 'https://github.com/CallumeEdwards777/Bootstrap-page.git',
+    },
+    {
+      id: 4,
+      title: 'Week 4 - Dynamic to do list using Javascript',
+      description: 'My fourth project implemented javascript and our task was to create an interactive to-do list that allows users to add and remove tasks',
+      tech: 'HTML, CSS, Javascript',
+      link: 'https://github.com/CallumeEdwards777/Dynamic-to-do-list-using-javascript.git',
+    },
+    {
+      id: 5,
+      title: 'Week 5 - Weather app using API',
+      description: 'This was my first time working as a part of a team to create a weather app using the open weather api. This project was a great learning experience as it taught me how to work in a team and how to use an api',
+      tech: 'HTML, CSS, Javascript, API',
+      link: 'https://github.com/krhdev/Darkside_Coders_CollabApp.git',
+    },
+    {
+      id: 6,
+      title: 'Week 8 - Full stack application for a tech blog',
+      description: 'I did not complete week 6 and 7 so it is straight to week 8 for my first time building a full stack application as a tech blog that included fully functional CRUD operations and user authentication',
+      tech: 'HTML, CSS, Javascript, Node.js, Express.js, MySQL',
+      link: 'https://github.com/CallumeEdwards777/Full-stack-application-for-a-tech-blog.git',
+    },
+];
+
+export default projects;
+    
+
+
+    
+  
